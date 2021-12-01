874dcf048dbab2ff516fe2e814e771f8844cf10b
connortomi <91751191+connortomi@users.noreply.github.com>
2021-12-01 11:09:46 -0800

    Add files via upload

---
+++ Project_1.3.1_CT.py
@@ -0,0 +1,18 @@
+import turtle as t 
+import tkinter as tk
+from playsound import playsound
+
+
+counter =  t.Turtle()
+
+def countdown():
+  global timer, timer_up
+  counter.clear()
+  if timer <= 0:
+    counter.write("Time's Up", font=font_setup)
+    timer_up = True
+    manage_leaderboard()
+  else:
+    counter.write("Timer: " + str(timer), font=font_setup)
+    timer -= 1
+    counter.getscreen().ontimer(countdown, counter_interval) 
